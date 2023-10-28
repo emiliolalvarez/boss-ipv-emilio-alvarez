0f498769344267851acc701d625388a3716fc788
emiliolalvarez <emiliolalvarez@gmail.com>
2023-10-27 22:02:51 -0300

    Add files via upload

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1698453213|21480042066";
+const CACHE_VERSION = "1698454898|23164792743";
 const CACHE_PREFIX = "Boss-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
