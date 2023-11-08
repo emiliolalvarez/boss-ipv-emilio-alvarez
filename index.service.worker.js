5c0c9cf01a1f53f6b482bdf58cda300aaef746e2
Emilio Alvarez <emilio.alvarez@avature.net>
2023-11-08 11:41:39 -0300

    udpate version

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1699448634|38309513215";
+const CACHE_VERSION = "1699454470|655421006";
 const CACHE_PREFIX = "Boss-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
