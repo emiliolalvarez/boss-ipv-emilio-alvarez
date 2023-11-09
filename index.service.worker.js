217390836f41fb721856b6670289391f96f9390a
Emilio Alvarez <emiliolalvarez@gmail.com>
2023-11-09 12:30:34 -0300

    new version

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1699501673|8990260689";
+const CACHE_VERSION = "1699543815|18434736874";
 const CACHE_PREFIX = "Boss-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
